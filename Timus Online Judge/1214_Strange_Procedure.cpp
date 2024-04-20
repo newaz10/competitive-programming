8d01e20224588a2a96c8e60f550b380a22a83a8e
newaz10 <mh.newaz001@gmail.com>
2024-04-20 20:23:16 +0600

    1214. Strange Procedure

--- Timus Online Judge/1214_Strange_Procedure.cpp
+++ Timus Online Judge/1214_Strange_Procedure.cpp
@@ -177,10 +177,30 @@ void solve()
     cin >> n;
 }
 
+void P(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        for (int i = 0; i < x + y; i++)
+        {
+            y = x * x + y;
+            x = x * x + y;
+            y = sqrt(x + y / abs(y) * -abs(y));
+            for (int j = 0; j < 2 * y; j++)
+                x -= y;
+        }
+    }
+    printf("%d %d\n", x, y);
+}
+
 int main()
 {
     FIO;
-    tc;
+    // tc;
+    int a, b;
+    cin >> a >> b;
+
+    P(a, b);
 
     return 0;
 }
