9c031c973a55df538f571fb0f2b92f5ba5a63ee3
MH Newaz <mh.newaz001@gmail.com>
2026-07-22 12:43:41 +0600

    solved: 2140B - Another Divisibility Problem

---
+++ CodeForces/B_Another_Divisibility_Problem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long x;
+    cin >> x;
+    
+    cout << 2*x << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
