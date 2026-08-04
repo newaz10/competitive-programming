e6c62bf8f410529e23e816ee8b1333f4020f214b
MH Newaz <mh.newaz001@gmail.com>
2026-08-04 18:45:16 +0600

    solved: 1339A - Filling Diamonds

---
+++ CodeForces/A_Filling_Diamonds.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long n;
+    cin >> n;
+    cout << n << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
